cfe17c61cb3edb07d0d991088cc94b8e879df680
NozimjonDD <nozimfromandijan@gmail.com>
2025-07-14 15:22:46 +0500

    Student improved

--- video_player/models.py
+++ video_player/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
+from common import utils
+from marks_projects.base_model import BaseModel
 
-# Create your models here.
+
+class LessonVideo(BaseModel):
+    title = models.CharField(max_length=100, help_text="Dars nomi", unique=True)
+    # lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name="videos")
+    # content = CKEditor5Field(null=True, blank=True, help_text="Dars matni")
+    video = models.FileField(upload_to='course/welcome/video/', null=True, blank=True, help_text="Dars video")
+    time = models.CharField(max_length=4, help_text="minutes", default=10)
+    image = models.ImageField(upload_to='course/welcome/images/', null=True, blank=True, help_text="Dars rasmi")
+    is_read = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, default=utils.auto_generate_slug())
+
+    def save(self, *args, **kwargs):
+        # generate slug if it's not already set or if thee title changesdd
+        if not self.slug or (self.pk and self.slug != slugify(self.title)):
+            base_slug = slugify(self.title)
+            unique_slug = base_slug
+            counter = 1
+            while LessonVideo.objects.filter(slug=unique_slug).exclude(pk=self.pk).exists():
+                unique_slug = f"{base_slug}-{get_random_string(6)}"  # Add a random string for uniqueness
+                counter += 1
+            self.slug = unique_slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
--- video_player/views.py
+++ video_player/views.py
@@ -1,3 +1,62 @@
+import os
+
+import requests
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
-# Create your views here.
+from video_player.models import LessonVideo
+
+'''
+HTML template for video player
+'''
+
+
+def secure_video_view(request, just_slug):
+    slug = request.headers.get('Referer').split('/')[-3]
+    model = request.headers.get('Referer').split('/')[-2]
+    name = 'test'
+    if model == 'video':
+        video = LessonVideo.objects.get(slug=slug)
+        name = video.video.name
+
+    #  Method-1 with local file path
+    # if not request.user.is_authenticated:
+    #     return HttpResponseForbidden("You are not allowed to access this video.")
+    # path = os.path.join(f"media/" f"{name}")
+    # return FileResponse(open(path, 'rb'), content_type='video/mp4')
+
+
+    #  Method-2 with file link(https://.........)
+    # url = "https://api.gossipgirl.uz/media/business/specialist/video_intro/5659273-hd_1080_1920_30fps.mp4"
+    local_filename = 'new_23.mp4'
+
+    #  If header needed
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+        # 'Referer': 'https://www.terabox.com',
+        # 'Accept': 'application/json, text/plain, */*',
+        # 'Cookie': 'your_cookie_here'  # TeraBox cookies-ni qo‘lda olish kerak
+    }
+
+    # response = requests.get(url, stream=True)
+    # response.raise_for_status()  # Raise an exception for HTTP errors
+
+    # with open(local_filename, 'wb') as f:
+    #     for chunk in response.iter_content(chunk_size=8192):
+    #         f.write(chunk)
+    #
+    # print(f"File downloaded successfully to {local_filename}")
+    #
+    # return FileResponse(open(local_filename, 'rb'), content_type='video/mp4')
+
+    #  Method-3 Stream with file link(https://.........) without save file local storage
+    from django.http import StreamingHttpResponse
+    #  url = domain.com/media/path/file.pm4
+    url = "https://vod3.cf.dmcdn.net/sec2(iXb5uzujxtrxAeFHfOhj9OWheMs2JhRlnXE-V1G9NmCAA1c6iizTl_SfMxvgfXrqVETBHHCW9QXsQLBJDeoHBt6Z7OHHVDqdcc2jyqL9kdQKpw347qTSvuCU9ARDheTz-P0xI6snlg-irSF02rsAHywQsZTELZiaoujj_D2uIzZryM7_4j16P96g9vmdX8F1jMXnxLFVD_a7-TdMI_Daow)/download/230/116/582611032_mp4_h264_aac_hq.mp4"
+    response = requests.get(url, stream=True)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    def stream_video():
+        for chunk in response.iter_content(chunk_size=8192):
+            yield chunk
+
+    return StreamingHttpResponse(stream_video(), content_type='video/mp4')
